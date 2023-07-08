157eacabd15b80cacb036e8116bb967674e9ee1f
GodVov4 <volume245@gmail.com>
2023-07-09 00:47:13 +0300

    Add function description

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # EasySort
 **Many people have a folder on their desktop called something like "Disassemble". As a rule, hands never manage to disassemble this folder.**
-
 ## How use?
 **1. Open terminal in script folder**
 ### * for Unix/MacOS
@@ -10,7 +9,10 @@ python -m pip install -r requirements.txt
 ```
 **3. Start script:**
 
-For example `python3 sort.py /Users/Username/Downloads/Your_folder`
+For example
+```
+python3 sort.py /Users/Username/Downloads/Your_folder
+```
 ### * for Windows
 **2. Install requirements:**
 ```
@@ -18,6 +20,9 @@ python -m pip install -r requirements.txt
 ```
 **3. Start script:**
 
-For example `python sort.py C:\Users\Username\Desctop\Your_folder`
+For example
+```
+python sort.py C:\Users\Username\Desctop\Your_folder
+```
 
 **4. Open** `Sort_result` **in your folder.**
--- sort.py
+++ sort.py
@@ -3,42 +3,24 @@ import shutil
 import sys
 from pathlib import Path
 from pyfiglet import Figlet
+from constants import NEW_FOLDERS, IGNORE_FOLDERS, EXTENSIONS, TRANSLIT
 
 
 f = Figlet(font='standard')
-user_path = Path(sys.argv[1])
-NEW_FOLDERS = {
-    'images': [],
-    'video': [],
-    'audio': [],
-    'documents': [],
-    'archives': []
-}
-EXTENSIONS = {
-    'images': ['JPEG', 'PNG', 'JPG', 'SVG', 'HEIC', 'ICO'],
-    'video': ['AVI', 'MP4', 'MOV', 'MKV', 'GIF'],
-    'audio': ['MP3', 'OGG', 'WAV', 'AMR', 'FLAC'],
-    'documents': ['DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'],
-    'archives': ['ZIP', 'GZ', 'TAR'],
-    'unknown': []
-}
-IGNORE_FOLDERS = {'archives', 'video', 'audio', 'documents', 'images'}
-TRANSLIT = {
-    1072: 'a', 1040: 'A', 1073: 'b', 1041: 'B', 1074: 'v', 1042: 'V', 1075: 'g', 1043: 'G', 1076: 'd', 1044: 'D',
-    1077: 'e', 1045: 'E', 1105: 'e', 1025: 'E', 1078: 'j', 1046: 'J', 1079: 'z', 1047: 'Z', 1080: 'y', 1048: 'Y',
-    1081: 'j', 1049: 'J', 1082: 'k', 1050: 'K', 1083: 'l', 1051: 'L', 1084: 'm', 1052: 'M', 1085: 'n', 1053: 'N',
-    1086: 'o', 1054: 'O', 1087: 'p', 1055: 'P', 1088: 'r', 1056: 'R', 1089: 's', 1057: 'S', 1090: 't', 1058: 'T',
-    1091: 'u', 1059: 'U', 1092: 'f', 1060: 'F', 1093: 'h', 1061: 'H', 1094: 'ts', 1062: 'TS', 1095: 'ch', 1063: 'CH',
-    1096: 'sh', 1064: 'SH', 1097: 'sch', 1065: 'SCH', 1098: '', 1066: '', 1099: 'y', 1067: 'Y', 1100: '', 1068: '',
-    1101: 'ye', 1069: 'YE', 1102: 'yu', 1070: 'YU', 1103: 'ya', 1071: 'YA', 1108: 'je', 1028: 'JE', 1110: 'i',
-    1030: 'I', 1111: 'ji', 1031: 'JI', 1169: 'g', 1168: 'G'
-}
+try:
+    user_path = Path(sys.argv[1])
+except IndexError:
+    print('You did\'t specify a folder path.\nTry again.')
+    exit()
+if not user_path.exists():
+    print('Invalid folder.\nTry again.')
+    exit()
 
 
 def normalize(path, translit):
     """
     Transliterate filename to english, remove problem symbols
-    :param path: file's path -> Path
+    :param path: file path -> Path
     :param translit: translit dict -> dict
     :return: normalized file -> Path
     """
@@ -46,9 +28,7 @@ def normalize(path, translit):
     ext = file[file.rfind('.'):]
     normalized_file = file.removesuffix(ext)
     normalized_file = normalized_file.translate(translit)
-    repl = re.findall(r'\W', normalized_file)
-    for i in repl:
-        normalized_file = normalized_file.replace(i, '_')
+    normalized_file = re.sub(r'\W', '_', normalized_file)
     normalized_file += ext
     new_path = path.parent / normalized_file
     if file != normalized_file:
@@ -60,9 +40,9 @@ def sort_groups(path):
     """
     Unpack all folders in path and sort by extensions
     :param path: user path -> Path()
-    :return: dict with new folders and files -> dict,
-            list of known extensions -> list,
-            list of unknown extensions -> list
+    :return: new folders and files dict -> dict,
+             known extensions list -> list,
+             unknown extensions list -> list
     """
     known_list = set()
     unknown_list = set()
@@ -84,7 +64,7 @@ def create_dirs(path, folders):
     """
     Create folders and move files
     :param path: user path -> Path
-    :param folders: dict with new folders and files -> dict
+    :param folders: new folders and files dict -> dict
     :return: None
     """
     for key, files in folders.items():
@@ -99,7 +79,7 @@ def create_dirs(path, folders):
             suffix = 1
             while new_file.exists():
                 text = file.name
-                text += str(suffix)
+                text = 'copy_' + str(suffix) + '_' + text
                 new_file = new_folder / text
                 suffix += 1
             shutil.move(file, new_file)
@@ -108,7 +88,7 @@ def create_dirs(path, folders):
 def unpacker(archive):
     """
     Unpack archives
-    :param archive: path of archive -> Path
+    :param archive: archive path -> Path
     :return: folder -> Path
     """
     name = archive
@@ -139,27 +119,27 @@ def delete_empty(path):
 
 def readme(new_folders, known_list, unknown_list):
     """
-    Create text file with description of results
-    :param new_folders: dict with new folders and files -> dict
-    :param known_list: list of known extensions -> list,
-    :param unknown_list: list of unknown extensions -> list
+    Create text file with results description
+    :param new_folders: new folders and files dict -> dict
+    :param known_list: known extensions list -> list,
+    :param unknown_list: unknown extensions list -> list
     :return: None
     """
-    path = Path(user_path) / 'Sort_result.txt'
+    path = Path(user_path) / 'SORT_RESULT.txt'
     with open(path, 'w') as result:
         result.write(f.renderText('EasySort'))
         result.write('\nCongratulations! You used the EasySort Sorter. Here\'s a list of what\'s been done:\n\n')
         for key, value in new_folders.items():
-            if len(value) == 0:
-                result.write(f'- In folder "{key}" nothing was moved.\n')
+            if not value:
+                result.write(f'* In folder "{key}" nothing was moved.\n')
             elif key != 'archives':
-                result.write(f'- In folder "{key}" was moved: {[i.name for i in value]}.\n')
+                result.write(f'* In folder "{key}" was moved: {[i.name for i in value]}.\n')
         for key, value in new_folders.items():
             if key == 'archives':
-                result.write(f'- Your archives {[i.name for i in value]} was unpacked in folder "{key}".\n')
-        result.write(f'\n- Known extensions: {known_list}.\n')
-        result.write(f'\n- Unknown extensions: {unknown_list}.\n')
-        result.write('\n- Your files have been transliterated.\n')
+                result.write(f'* Your archives {[i.name for i in value]} was unpacked in folder "{key}".\n')
+        result.write(f'\n* Known extensions: {known_list}.\n')
+        result.write(f'\n* Unknown extensions: {unknown_list}.\n')
+        result.write('\n* Your files have been transliterated.\n')
         result.write('\n\nProduced in Ukraine by Volodymyr Martyn.\n')
 
 
@@ -168,6 +148,7 @@ def main():
     create_dirs(user_path, new_folders)
     delete_empty(user_path)
     readme(new_folders, known_list, unknown_list)
+    print('\nDone! Your files was sorted. Open "SORT_RESULT.txt" for see results.\n')
 
 
 if __name__ == '__main__':
